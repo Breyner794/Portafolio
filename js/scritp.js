e3f64adfdf2efc71690c16e495b6a660912c0452
Breyner794 <83435124+Breyner794@users.noreply.github.com>
2024-11-19 22:58:26 -0500

    Primer Avance
    Este es el primer avance para mi portafolio, se debe de organizar el primer evento que es sobre mi

---
+++ js/scritp.js
@@ -0,0 +1,12 @@
+const temaOscuro = ()=>{
+    document.querySelector("body").setAttribute("data-bs-theme", "dark");
+    document.querySelector("#dl-icon").setAttribute("class", "bi bi-sun-fill");
+}
+const temaClaro = ()=>{
+    document.querySelector("body").setAttribute("data-bs-theme", "light");
+    document.querySelector("#dl-icon").setAttribute("class", "bi bi-moon-fill");
+}
+const temaActual = ()=>{
+    document.querySelector("body").getAttribute("data-bs-theme") === "light"?
+    temaOscuro() : temaClaro();
+}
